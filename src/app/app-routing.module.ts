85199fc5e46f02564799b8a8e31abe25e3c7286e
AlexadreNoguez <alexandrenoguez_rs@yahoo.com.br>
2024-01-16 21:37:16 -0300

    fix: changing app-routes and adding not-found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth.guard';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
+
   {
-    path: 'sign-in',
-    loadChildren: () => import("./auth/sign-in/sign-in.module")
-      .then(m => m.SignInModule)
+    path: 'auth',
+    children: [
+      { path: 'sign-up', loadChildren: () => import("./auth/sign-up/sign-up.module").then(m => m.SignUpModule), },
+      { path: "sign-in", loadChildren: () => import("./auth/sign-in/sign-in.module").then(m => m.SignInModule), },
+    ],
   },
   {
-    path: '', loadChildren: () => import("./pages/home/home.module")
-      .then(m => m.HomeModule),
+    path: '', loadChildren: () => import("./pages/home/home.module").then(m => m.HomeModule),
     canActivate: [AuthGuard]
-  }
+  },
+  { path: '404', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/404' }
 ];
 
 @NgModule({
