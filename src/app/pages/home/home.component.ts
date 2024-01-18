0323aa3a5d571b9e2e7f72b146254ec974f56c78
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2024-01-18 10:36:35 -0300

    feat: added home with header to nav in the application

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {JobModel} from "../../types/JobTypes";
+import {JobService} from "../../service/job.service";
+import {Subject} from "rxjs";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(
+    private jobService: JobService
+  ) {
+    this.onGetAllJobs();
+
+  }
+  jobs: JobModel[] = []
+  userSearch: string = ""
+
+  ngOnInit() {
+    this.onGetAllJobs();
+    console.log("jobs",this.jobs)
+
+  }
+  onGetAllJobs(): void {
+    this.jobService.getAllJobs("").subscribe(
+      (response: any) => {
+        console.log("response", response)
+        this.jobs = response?.elements || [];
+      },
+      (error) => {
+        console.error('Error fetching jobs:', error);
+      }
+    );
+  }
+
+  onApplyCandidate(): void {
+
+
+  }
 
 }
