fc088ff192bd11be4dbde74a70a3a6a3ab941310
AlexadreNoguez <alexandrenoguez_rs@yahoo.com.br>
2024-01-22 01:29:27 -0300

    feat: search filter by job title

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -5,11 +5,13 @@ import {HomeRoutingModule} from './home-routing.module';
 import {HeaderComponent} from 'src/app/components/header/header.component';
 import {RoleCardComponent} from "../../components/role-card/role-card.component";
 import {FormsModule} from "@angular/forms";
+import {SearchFilterPipe} from "../../pipes/search-filter.pipe";
 
 
 @NgModule({
   declarations: [
     HomeComponent,
+    SearchFilterPipe
   ],
   imports: [
     CommonModule,
--- src/app/pipes/search-filter.pipe.ts
+++ src/app/pipes/search-filter.pipe.ts
@@ -1,19 +1,20 @@
-import {Pipe, PipeTransform} from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
+import {JobModel} from "../types/JobTypes";
 
 @Pipe({
   name: 'searchFilter'
 })
 export class SearchFilterPipe implements PipeTransform {
 
-  // transform(value: unknown, ...args: unknown[]): unknown {
-  //   return null;
-  // }
-  transform(items: any, searchTerm: any): any[] {
-    if (!searchTerm) {
-      return items;
-    }
+  transform(value: JobModel[], args?: string): JobModel[] {
+    if(!value) return [];
+    if(!args) return value;
 
-    return items.filter((item: string) => item.toLowerCase().includes(searchTerm));
+    args = args.toLowerCase();
+
+    return value.filter((job: JobModel) => {
+      return job.title.toLowerCase().includes(args!)
+    })
   }
 
 }
